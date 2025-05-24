2d5ba80651b88d6f205451332f08e7b0c252997e
Joshua Dascoli <joshuadascoli@gmail.com>
2025-05-24 13:09:50 -0500

    Setting up home screen

--- CardPlay/app/index.tsx
+++ CardPlay/app/index.tsx
@@ -1,15 +1,12 @@
-import { Text, View } from "react-native";
+import { Button, View } from "react-native";
+import { useRouter } from 'expo-router';
 
-export default function Index() {
+export default function HomeScr() {
+  const router = useRouter();
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={{flex: 1, justifyContent: "center", alignItems: "center",}}>
+      <Button title="Singleplayer" onPress={() => router.push('/singleplayer')} />
     </View>
   );
 }
---
+++ CardPlay/app/singleplayer.tsx
@@ -0,0 +1,7 @@
+import {View } from "react-native";
+
+export default function Singleplayer() {
+    return(<View>
+        Begin game
+    </View>);
+}
