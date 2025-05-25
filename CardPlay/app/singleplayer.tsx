6460524608f0c879660eb0a5b5c91d3bd1b93916
Joshua Dascoli <joshuadascoli@gmail.com>
2025-05-25 01:23:03 -0500

    Setting up animations

--- CardPlay/app/singleplayer.tsx
+++ CardPlay/app/singleplayer.tsx
@@ -1,30 +1,36 @@
 import { useState } from "react";
-import { Pressable, HStack, Image } from "react-native-ficus-ui";
+import { Pressable, HStack, Image, Box } from "react-native-ficus-ui";
 
 export default function Singleplayer() {
 
-    const [coin,setCoin] = useState('heads');
+    const [coin, setCoin] = useState('heads');
+    const [flipping, setFlipping] = useState(false);
 
     const Flip = () => {
+        setFlipping(true);
         const num = Math.floor(Math.random() * 2);
-        if(num === 1) {
+        if (num === 1) {
             console.log("heads");
-            if(coin === "heads") console.log("you go first");
+            if (coin === "heads") console.log("you go first");
             else console.log("you go second");
         }
         else {
             console.log("tails");
-            if(coin === "tails") console.log("you go first");
+            if (coin === "tails") console.log("you go first");
             else console.log("you go second");
         }
     }
 
-    return (<HStack style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Pressable bg="black" borderRadius="xl" onPress={() => { console.log("You pressed heads"); setCoin("heads"); Flip(); }}>
-            <Image h={180} w={180} source={require('../assets/images/heads.png')} />
-        </Pressable>
-        <Pressable bg="black" borderRadius="xl" onPress={() => { console.log("You pressed tails"); setCoin("tails"); Flip(); }}>
-            <Image h={180} w={180} source={require('../assets/images/tails.png')} />
-        </Pressable>
-    </HStack>);
+    return (
+        <Box style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+            {flipping ? (<video width="640" height="360" controls><source src='../assets/videos/Heads.png' type="video/mp4" /></video>) : null}
+            <HStack>
+                <Pressable bg="black" borderRadius="xl" onPress={() => { console.log("You pressed heads"); setCoin("heads"); Flip(); }}>
+                    <Image h={180} w={180} source={require('../assets/images/head.png')} />
+                </Pressable>
+                <Pressable bg="black" borderRadius="xl" onPress={() => { console.log("You pressed tails"); setCoin("tails"); Flip(); }}>
+                    <Image h={180} w={180} source={require('../assets/images/tails.png')} />
+                </Pressable>
+            </HStack>
+        </Box>);
 }
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # RebellionAgainstTheKing
 A simple card game which challenges both the players to a test of wits and intraspection
+
+## Go into project
+```bash
+   cd CardPlay
+```
